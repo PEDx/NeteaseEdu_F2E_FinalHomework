20d7e3108687c793fe94654a6e76c31954f26c4c
PED <pedclub@163.com>
2016-07-03 19:19:47 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -167,8 +167,8 @@ var STEP=5000;
 (function(){
     //关闭顶部小条
     var ele=document.getElementById('header');
-     if(getCookie("hasClosed")==1){
-        addClass(ele,"closed");
+     if(getCookie("hasClosed")!==1){
+        delThenAdd(ele,"open","closed");
     }
     document.getElementById("close").onclick=function(){
         addClass(ele,"closed");
